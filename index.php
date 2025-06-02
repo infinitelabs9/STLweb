02cc76d34fd2f233a9d968f420b19db6b0712bc6
infinitelabs9 <infinitenetlabs@gmail.com>
2025-06-02 13:27:05 +0800

    home page and style of homepage terminado

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
   <div class="conte-body">
 
     <div class="conte-header">
-      <div class="subcon1-0">STLclub</div>
+    <div class="subcon1-0">
+  STL<span class="highlight">club</span>
+</div>
       <div class="menu">
         <div class="caja">Home</div>
         <div class="caja">Post</div>
@@ -24,11 +26,102 @@
     <div class="imagen-text">
       <img src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
       <div class="text-inside">Fueling the Future,<br> One Launch at a </br>Time.</br>
+      <p class="text-inside1-1"> Join STL, Tamkang University’s Rocketry Club, as we design, build, and launch high-powered rockets that push the limits of student innovation. From local projects to international competitions, we’re a team of dreamers, engineers, and explorers committed to shaping the future of aerospace.</p>
+      <button class="button1">Apply</button>
     </div>
-    
     </div>
+    <div class="organization-div"> 
+  <h1 class="h1text">Organization Team</h1>
+  <h3 class="h3text">Check our Team Organization </h3>
+
+  <div class="sub-organization">
+    <div class="team-con">
+      <img class="size-img" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <p class="team-name">System Division</p>
+    </div>
+    <div class="team-con">
+      <img class="size-img" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <p class="team-name">Desing</p>
+    </div>
+    <div class="team-con">
+      <img class="size-img" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <p class="team-name">Structure</p>
+    </div>
+    <div class="team-con">
+      <img class="size-img" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <p class="team-name">Avionic</p>
+    </div>
+    <div class="team-con">
+      <img class="size-img" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <p class="team-name">Propulsion</p>
+    </div>
+    <div class="team-con">
+      <img class="size-img" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <p class="team-name">Parachute</p>
+    </div>
+
 
   </div>
+  <h1 class="h1text">Latest Strories</h1>
+<div class = "story-con"> 
+  <div class="story-con-1">
+    <div class="div-1">
+      <img class="size-img2" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <h1 class="div-2text">How we Built our Hybird Rocket for the 2024 competion.</h1>
+      <h3 class="div-3text">You can join to the team which fit bette with you.</h3>
+    </div>
+  </div>
+  <div class="story-con-2">
+     <div class="div-2">
+      <img class="size-img3" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+         <div class="subchil">
+      <h1 class="div-2text">Meet the STL team behind the engineers.</h1>
+      <h3 class="div-3text">You can join to the team which fit bette with you</h3>
+         </div>
+     </div>
+     <div class="div-2">
+      <img class="size-img3" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+         <div class="subchil">
+         <h1 class="div-2text">Rocket progress</h1>
+         <h3 class="div-3text">You can join to the team which fit bette with you</h3>
+         </div>
+     </div>
+     <div class="div-2">
+      <img class="size-img3" src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+         <div class="subchil">
+         <h1 class="div-2text">Launch day!</h1>
+         <h3 class="div-3text">You can join to the team which fit bette with you</h3>
+         </div>
+     </div>
+     
+  </div>
 
+</div>
+<div class="footer-con">  
+  <h1 class="foot-sub1">
+    STL<span class="highlight2">club</span>
+  </h1>
+
+  <div class="foot-sub2">
+    <h1>Follow us</h1>
+    <div class="foot-div2">Apply</div>
+  </div>
+
+  <div class="foot-sub3">
+    <h2 class="foot-div">Post</h2>
+    <h2 class="foot-div">Apply</h2>
+    <h2 class="foot-div">About Us</h2>
+  </div>
+</div>
+
+  
+
+</div>
+
+<div class="foot-sub"></div>
+
+
+
+  </div>
 </body>
 </html>
